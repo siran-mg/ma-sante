45237aedffa69cebd3c01bcf4788bba194065dc8
Naris Razafimahatratra <naris.razafimahatratra@gmail.com>
2024-11-13 17:42:31 +0300

    💄 Doctors list

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,11 +1,32 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:ma_sante/home/carousel.dart';
+import 'package:ma_sante/home/list_item.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Carousel();
+    final random = Random();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Carousel(),
+          ...List.generate(
+            10,
+            (index) => ListItem(
+              imageUrl:
+                  'https://picsum.photos/200/300?random=${random.nextInt(100)}',
+              name: 'Nom',
+              firstName: 'Prénom',
+              speciality: 'Psychologue',
+              location: 'Lieu de travail',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/home/list_item.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class ListItem extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String firstName;
+  final String speciality;
+  final String location;
+
+  const ListItem({
+    super.key,
+    required this.imageUrl,
+    required this.name,
+    required this.firstName,
+    required this.speciality,
+    required this.location,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 70,
+                        height: 80,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            imageUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '$firstName $name',
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                ),
+                                Text(speciality,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium),
+                                Row(children: [
+                                  Icon(
+                                    Icons.location_on,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                  Text(location,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium),
+                                ]),
+                              ],
+                            ),
+                            IconButton.filled(
+                              onPressed: () {},
+                              icon: const Icon(Icons.call),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text("15"),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Icon(Icons.thumb_up),
+                        ],
+                      ),
+                      Text("1 commentaire"),
+                    ],
+                  ),
+                  const Divider(),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.thumb_up_outlined),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("J'aime")
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.comment),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("Commenter")
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
